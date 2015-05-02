25189188c430779b95514f64135cad7b83b45b43
NickMyers217 <nickbmyers217@gmail.com>
2015-05-01 23:55:47 -0500

    Finished the first five problems

--- euler.hs
+++ euler.hs
@@ -1,41 +1,63 @@
 --Problem 1
-	--List comprehension
-	euler1 :: (Integral a) => a -> a
-	euler1 cap = sum [ n | n <- [1..cap - 1], mod n 3 == 0 || mod n 5 == 0 ]
+    --List comprehension
+    euler1 :: (Integral a) => a -> a
+    euler1 cap = sum [ n | n <- [1..cap - 1], mod n 3 == 0 || mod n 5 == 0 ]
 
-	--Recursion with guards
-	euler1' :: (Integral a) => a -> a -> a
-	euler1' n cap
-		| n == cap 							= 0
-		| n `mod` 3 == 0 || n `mod` 5 == 0 	= n + euler1' (n + 1) cap
-		| otherwise 						= euler1' (n + 1) cap
+    --Recursion with guards
+    euler1' :: (Integral a) => a -> a -> a
+    euler1' n cap
+        | n == cap                         = 0
+        | n `mod` 3 == 0 || n `mod` 5 == 0 = n + euler1' (n + 1) cap
+        | otherwise                        = euler1' (n + 1) cap
 
 
 --Problem 2
-	fibSum :: (Integral a) => a -> a -> a -> a
-	fibSum a b cap
-		| c >= cap 			= 0
-		| c `mod` 2 == 0 	= c + fibSum b c cap
-		| otherwise			= fibSum b c cap
-		where c = a + b
+    fibSum :: (Integral a) => a -> a -> a -> a
+    fibSum a b cap
+        | c >= cap       = 0
+        | c `mod` 2 == 0 = c + fibSum b c cap
+        | otherwise      = fibSum b c cap
+        where c = a + b
 
-	euler2 :: (Integral a) => a -> a
-	euler2 n = fibSum 1 1 n
+    euler2 :: (Integral a) => a -> a
+    euler2 n = fibSum 1 1 n
 
 
 --Problem 3
-	lgstPrimeFac :: (Integral a) => a -> a -> a
-	lgstPrimeFac num factor
-		| factor^2 > num		= num
-		| num `mod` factor == 0	= lgstPrimeFac (num `div` factor) factor
-		| otherwise				= lgstPrimeFac num (factor + 1)
+    lgstPrimeFac :: (Integral a) => a -> a -> a
+    lgstPrimeFac num factor
+        | factor^2 > num        = num
+        | num `mod` factor == 0 = lgstPrimeFac (num `div` factor) factor
+        | otherwise             = lgstPrimeFac num (factor + 1)
 
-	euler3 :: (Integral a) => a -> a
-	euler3 n = lgstPrimeFac n 2
+    euler3 :: (Integral a) => a -> a
+    euler3 n = lgstPrimeFac n 2
 
 
 --Problem 4
-	euler4 = lgstPalindrome 100 999
-	lgstPalindrome strt cap = maximum (filter isPalindrome [ a * b | a <- [strt..cap], b <- [a..cap] ])
-		where isPalindrome n = show n == reverse (show n)
+    euler4 = lgstPalindrome 100 999
+    lgstPalindrome strt cap = maximum (filter isPalindrome [ a * b | a <- [strt..cap], b <- [a..cap] ])
+        where isPalindrome n = show n == reverse (show n)
+
+--Problem 5
+    --Came up with this solution first
+    isDivByAll :: (Integral a) => a -> [a] -> Bool
+    isDivByAll n [] = True
+    isDivByAll n (x:xs)
+        | n `mod` x /= 0 = False
+        | otherwise      = n `isDivByAll` xs
+
+    euler5 :: (Integral a) => a -> [a] -> a
+    euler5 n divisors
+        | n `isDivByAll` divisors = n
+        | otherwise               = euler5 (n + 1) divisors
+
+    --It can be done with this one liner though
+    --It folds the least common multiple function across the list of divisors
+    --Normally it takes in two inputs and finds the lcm, now it finds the lcm of the whole list
+    euler5' :: (Integral a) => [a] -> a
+    euler5' divisors = foldr lcm 1 divisors
+
+
+
 
